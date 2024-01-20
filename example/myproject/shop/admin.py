cc2f0def2acb7a1d3f6e363b4e62f1220ddc9440
Shinya Okano <shinya.okano@open-c.jp>
2024-01-21 02:08:25 +0900

    発注データのモデル

--- example/myproject/shop/admin.py
+++ example/myproject/shop/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Item)
+admin.site.register(models.OrderedItem)
+admin.site.register(models.PurchaseOrder)
--- example/myproject/shop/models.py
+++ example/myproject/shop/models.py
@@ -6,6 +6,25 @@ class Item(models.Model):
 
     name = models.CharField("品名", max_length=50)
     price = models.PositiveIntegerField("価格", default=0)
+    code = models.CharField("品番", max_length=4, default="0000")
 
     def __str__(self):
-        return f"{self.pk}:{self.name}"
+        return f"{self.pk}:{self.code}:{self.name}"
+
+
+class OrderedItem(models.Model):
+    """発注された商品"""
+    name = models.CharField("品名", max_length=50)
+    price = models.PositiveIntegerField("価格", default=0)
+    code = models.CharField("品番", max_length=4, default="0000")
+
+    def __str__(self):
+        return f"{self.code}:{self.name}"
+
+
+class PurchaseOrder(models.Model):
+    """注文
+    """
+    from_name = models.CharField("注文者名", max_length=50)
+    ordered_at = models.DateTimeField("注文日時", auto_now_add=True)
+    ordered_items = models.ManyToManyField(OrderedItem, "発注に含まれる商品")
