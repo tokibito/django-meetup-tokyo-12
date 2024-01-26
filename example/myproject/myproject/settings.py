eb5137b1a1dbedddfd20e9522fb947aa2f6bf6ee
Shinya Okano <shinya.okano@open-c.jp>
2024-01-27 01:11:20 +0900

    django-debug-toolbar

--- example/myproject/myproject/settings.py
+++ example/myproject/myproject/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "shop",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -122,3 +124,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- example/myproject/myproject/urls.py
+++ example/myproject/myproject/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("shop.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
